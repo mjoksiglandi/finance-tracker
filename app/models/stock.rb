69ac0477ceb5a7566a78f1db01417bf3b5aa745e
Gormm <juan.cornejo.s@gmail.com>
2021-08-15 23:55:19 -0400

    actualizado rubi 2.6.8 a 2.7.3

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,8 @@
 class Stock < ApplicationRecord
+
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(publishable_token: 'Tpk_fbaee31984504b589e9ed2e943c96692',
+                                  endpoint: 'https://sandbox.iexapis.com/v1')
+  client.price(ticker_symbol)
+  end
 end
