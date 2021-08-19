df37bd15814b3862362c7dca64f0178e7a578db5
Gormm <juan.cornejo.s@gmail.com>
2021-08-18 21:59:56 -0400

    add stock modedl and iex api key

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,7 +1,7 @@
 class Stock < ApplicationRecord
 
   def self.new_lookup(ticker_symbol)
-    client = IEX::Api::Client.new(publishable_token: 'Tpk_fbaee31984504b589e9ed2e943c96692',
+    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
                                   endpoint: 'https://sandbox.iexapis.com/v1')
   client.price(ticker_symbol)
   end
