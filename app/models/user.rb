f7becf0e0ce0fe5b446c87a9bb524ffc574babf2
Gormm <juan.cornejo.s@gmail.com>
2021-08-21 21:45:59 -0400

    users and stock associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :user_stocks
+  has_many :stocks, through: :user_stocks
+
+  validates :name, :thicker, presence: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
