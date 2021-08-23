ace8c1ec83ad3436d86902445c63c829e8af9ad8
Gormm <juan.cornejo.s@gmail.com>
2021-08-22 21:20:13 -0400

    add stock tracking restrictions

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -5,18 +5,18 @@ class StocksController < ApplicationController
       @stock = Stock.new_lookup(params[:stock])
       if @stock
         respond_to do |format|
-          format.js{render partial: 'users/result'}
+          format.js { render partial: 'users/result' }
         end
       else
         respond_to do |format|
           flash.now[:alert] = "Please enter a valid symbol to search"
-          format.js{render partial: 'users/result'}
+          format.js { render partial: 'users/result' }
         end
       end
     else
       respond_to do |format|
         flash.now[:alert] = "Please enter a symbol to search"
-        format.js{render partial: 'users/result'}
+        format.js { render partial: 'users/result' }
       end
     end
   end
